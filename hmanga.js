95b2f95d7900a442c0110a1beedddc64fdb503e7
Hendrik Wallbaum <mail@hendrikwallbaum.de>
2016-03-30 17:04:16 +0800

    Started with a reamde

--- hmanga.js
+++ hmanga.js
@@ -1,4 +1,5 @@
 var cli = require('commander');
+var scraper = require('./hmanga-scrape');
 
 //Prettier output https://www.npmjs.com/package/cli-color
 //Parsing commandline input using https://www.npmjs.com/package/commander
@@ -6,10 +7,11 @@ var cli = require('commander');
 cli
     .usage('[options] <url ...>')
     .option('-i, --information', 'Updates the information saved about all downloaded mangas.')
+    .option('-u, --update', 'Search for updates to mangas.')
     .command('serve', 'Start a website to view downloaded mangas. (coming soon)')
     .parse(process.argv);
 
-var scraper = require('./hmanga-scrape');
+
 if(cli.information) {
     //TODO update infos
 } else if(cli.args[0]) {
---
+++ readme.md
@@ -0,0 +1,19 @@
+## Usage
+
+To download a manga run `hmanga <url to manga>`. This will download the manga into a sub-folder of the current directory. You can specify any url associated with a manga. Leaving the url blank will instead look for updates to all previously downloaded mangas.
+
+```
+Usage: hmanga [options] <url ...>
+
+ Options:
+
+   -h, --help         output usage information
+   -i, --information  Updates the information saved about all downloaded mangas.
+   -u, --update       Search for updates to mangas.
+ ```
+
+### hmanga serve
+
+Starts a website that you can use to view all your mangas.
+
+Simply run `hmanga serve`.
