c4eccd3dcddf4ff4987256fecb097185d71d244c
Hendrik Wallbaum <mail@hendrikwallbaum.de>
2016-04-01 14:18:26 +0800

    Setting up routes

--- hmanga-serve.js
+++ hmanga-serve.js
@@ -4,7 +4,44 @@ function startServer() {
 
     app.get('/manga/:manga', function(req, res) {
         var dir = req.params.manga.replace(/-/g, ' ');
-        var config = require(`./${dir}/${dir}.json`);
+        try {
+            var config = require(`./${dir}/${dir}.json`);
+            res.status(200).json(config).end();
+        } catch (e) {
+            res.status(404).end();
+        }
+    });
+
+    app.get('/manga/:manga/:chapter/:page', function(req, res) {
+        var dir = req.params.manga.replace(/-/g, ' ');
+        try {
+            var config = require(`./${dir}/${dir}.json`);
+            var chapter = req.params.chapter;
+            var page = req.params.page;
+            if (config.chapters[chapter].pages >= page) {
+                var options = {
+                    root: __dirname + '/public/',
+                    dotfiles: 'deny',
+                    headers: {
+                        'x-timestamp': Date.now(),
+                        'x-sent': true
+                    }
+                };
+                var fileName = `${config.dir}/Chapter ${chapter}/${config.name} ch.${chapter} pg.${page}.jpg`;
+                res.sendFile(fileName, options, function(err) {
+                    if(err) {
+                        console.log(err);
+                    }
+                });
+            } else {
+
+                //This chapter or page does not exist.
+                res.status(704).end();
+            }
+        } catch (e) {
+            console.log('Not existing manga requested.');
+            res.status(404).end();
+        }
     });
 
     app.use(express.static('public'));
