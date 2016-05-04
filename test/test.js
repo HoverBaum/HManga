4443184a7d0f1252a2a54f5246cf4792e1743019
Hendrik Wallbaum <mail@hendrikwallbaum.de>
2016-05-04 18:27:00 +0800

    setup basic files for testing

---
+++ test/test.js
@@ -0,0 +1,5 @@
+
+var test = require('tape');
+
+var testMerger = require('./testMerger');
+testMerger(test);
---
+++ test/testMerger.js
@@ -0,0 +1,6 @@
+module.exports = function(test) {
+    test('Merger', function(t) {
+        t.pass('works');
+        t.end();
+    });
+}
