99766ee8e8f7d198d60970804c7a2b0053be0d72
Hendrik Wallbaum <mail@hendrikwallbaum.de>
2016-05-13 13:40:30 +0800

    add progress bar and fake data to chapter page

--- publicSRC/js/chapter.js
+++ publicSRC/js/chapter.js
@@ -1,14 +1,38 @@
 import React from 'react';
 import AppBar from 'material-ui/AppBar';
 import Page from './page';
+import Helper from './helper';
+import LinearProgress from 'material-ui/LinearProgress';
 
 export default class Chapter extends React.Component{
 
 	constructor(props) {
 		super(props);
 		this.state = {
-			page: 1
+			page: 1,
+			src: 'test.jpg',
+			chapter: {}
 		}
+		let {chapter, mangaName} = this.props.params;
+		this.getChapterInfo(mangaName, chapter);
+		window.addEventListener('keydown', this.handleKeys);
+	}
+
+	getChapterInfo = (mangaName, chapterNumber) => {
+		var that = this;
+		Helper.get('/testManga.json', function(data) {
+			data = JSON.parse(data);
+			let returnChapter = data.chapters[chapterNumber];
+			that.setState({chapter: returnChapter});
+		});
+	}
+
+	nextPage = () => {
+
+	}
+
+	prevPage = () => {
+
 	}
 
 	render() {
@@ -18,8 +42,9 @@ export default class Chapter extends React.Component{
 			<div>
 
 			    <AppBar title={appBarTitle} />
+				<LinearProgress mode="determinate" value={this.state.page} max={this.state.chapter.totalPages} />
 
-				<Page page={this.state.page} />
+				<Page src={this.state.src} onNextPage={this.nextPage} onPrevPage={this.prevPage} />
 
 			</div>
 		)
--- publicSRC/js/page.js
+++ publicSRC/js/page.js
@@ -2,11 +2,27 @@ import React from 'react';
 
 export default class Page extends React.Component{
 
+	constructor(props) {
+		super(props);
+		window.addEventListener('keydown', this.handleKeys);
+	}
+
+	handleKeys = (e) => {
+		//39 right, 37 left, 27 ESC, 13 ENTER, 32 SPACE, 68 d, 65 a
+		let code = e.keyCode;
+		if(code === 39 || code === 68) {
+			this.props.onNextPage();
+		} else if(code === 37 || code === 65) {
+			this.props.onPrevPage();
+		}
+	}
+
 	render() {
+
 		return (
 			<div>
 
-			    <img src="test.jpg" className="single-page" />
+			    <img src={this.props.src} className="single-page" />
 
 			</div>
 		)
